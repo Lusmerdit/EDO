e6f8c00db29564bf650e12c84ea3570312a2b1e4
Lusmerdit <Ludovic.Godard-Cadillac@bordeaux-inp.fr>
2024-10-01 09:33:34 +0200

    new file RollCall

---
+++ RollCall.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 09:28:37 2024
+
+@author: lgcadillac
+"""
+
+import sys
+
+FileName = "2Acpp.txt"
+File = open(FileName, "r")
+
+print(File)
+
+StudentList=[]
+
+File.close()
