aa66996c762400f91ae834c4d9cdc854e702433b
GODARD-CADILLAC Ludovic <Ludovic.Godard-Cadillac@bordeaux-inp.fr>
2024-12-09 18:07:11 +0100

    save plot convergence order

--- Error.py
+++ Error.py
@@ -20,20 +20,21 @@ def dF(t,x):
 def Solution(t,x_0):
     return (x_0+1./5)*exp(-3.*t)+sin(9.*t)/15-cos(9*t)/5
 
-exact=False
+exact=True
 
 if exact :
 
     dt, e_ee, e_ei, e_cn, e_rk4, e_h, e_ab2, e_rk3 = TraceErreur(F,dF,Solution)
             
     #tracé échelle log-log choix couleurs : 'b','c','g','k','m','r','y','w'
-    loglog(dt,e_ee,'.b')
-    loglog(dt,e_ei,'.r')
-    loglog(dt,e_cn,'.g')
-    loglog(dt,e_h,'.y')
-    loglog(dt,e_ab2,'.c')
-    loglog(dt,e_rk3,'.b')
-    loglog(dt,e_rk4,'.m')
+    #loglog(dt,e_ee,'.b')
+    loglog(dt,e_ei,'x-k')
+    #loglog(dt,e_cn,'.g')
+    loglog(dt,e_h,'o-k')
+    loglog(dt,e_ab2,'s-k')
+    loglog(dt,e_rk3,'-k')
+    #loglog(dt,e_rk4,'.m')
+    savefig("save.png")
 
 else :
     dt, e_ee, e_ei, e_cn, e_rk4, e_h, e_ab2, e_rk3 =TraceErreurApprox(F,dF)
