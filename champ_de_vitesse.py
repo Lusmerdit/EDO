3e496bab4fc16752d1df5a640837e200f089777a
Godard-cadillac Ludovic <lgcadillac@o7460-15.math.u-bordeaux1.fr>
2025-08-29 09:42:17 +0200

    champ 3D

--- champ_de_vitesse.py
+++ champ_de_vitesse.py
@@ -6,6 +6,27 @@ Created on Fri Aug 29 09:19:45 2025
 @author: lgcadillac
 """
 
-from pylab import *
 from Schemas import *
+import matplotlib.pyplot as plt
+import numpy as np
+from sys import exit
 
+ax = plt.figure().add_subplot(projection='3d')
+
+# Make the direction data for the arrows
+x, y, z = np.meshgrid(np.arange(0., 1., 0.1),
+                      np.arange(0., 1., 0.1),
+                      np.arange(0., 1., 0.1))
+
+u = -y
+v = x
+w = -z
+
+def f(x,y,z):
+    return [-y,x,1+0.2*z]
+
+
+
+ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True, color = 'k')
+
+plt.show()
---
+++ champ_de_vitesse_3D.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 29 09:41:38 2025
+
+@author: lgcadillac
+"""
+
