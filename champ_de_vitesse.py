3e350836a9f35239238ba7132a03b4e0e8ce81db
Godard-cadillac Ludovic <lgcadillac@o7460-15.math.u-bordeaux1.fr>
2025-08-29 11:21:42 +0200

    functionnal champ de vitesse

--- champ_de_vitesse.py
+++ champ_de_vitesse.py
@@ -9,24 +9,46 @@ Created on Fri Aug 29 09:19:45 2025
 from Schemas import *
 import matplotlib.pyplot as plt
 import numpy as np
-from sys import exit
 
-ax = plt.figure().add_subplot(projection='3d')
+# Le choix du champ de vitesse F :
+def F(t,V):
+    return np.array([(1-4*(V[0]+V[1])**2)/sqrt((1-4*(V[0]+V[1])**2)**2+(1+2*(V[0]-V[1]))**2+0.1), 
+                     (1+2*(V[0]-V[1]))/sqrt((1-4*(V[0]+V[1])**2)**2+(1+2*(V[0]-V[1]))**2+0.1)])
 
-# Make the direction data for the arrows
-x, y, z = np.meshgrid(np.arange(0., 1., 0.1),
-                      np.arange(0., 1., 0.1),
-                      np.arange(0., 1., 0.1))
+# Le calcul de sa derivee (pour Newton) :
+def dF(t,X):
+    return 0
 
-u = -y
-v = x
-w = -z
+X, Y = np.meshgrid(np.arange(-1, 1, 0.1), np.arange(-1, 1, 0.1))
+U, V = np.meshgrid(np.arange(-1, 1, 0.1), np.arange(-1, 1, 0.1))
 
-def f(x,y,z):
-    return [-y,x,1+0.2*z]
+for i in range(len(X)):
+    for j in range(len(X[i])):
+        U[i][j] = F(0,[X[i][j],Y[i][j]])[0]
+        V[i][j] = F(0,[X[i][j],Y[i][j]])[1]
+        
+fig, ax = plt.subplots()
+q = ax.quiver(X, Y, U, V, pivot='mid', scale =25)
 
+plt.cla()
+# Choix du schéma numérique : EE, EI, CN, Heun, RK3, RK4, AB2, PMI
+solve = RK4
 
+# Paramètres pour l'intégration numérique
+dt=0.01
+T=5.
+x_0=np.array([0.12,-1.])
+N=int(ceil(T/dt))
+dt=T/N
 
-ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True, color = 'k')
+# Resolution numérique de l'équation :
+t,V=solve(F,dF,dt,T,x_0) # solution numérique
+    
+A=[I[0] for I in V]
+B=[I[1] for I in V]
+
+plot(A, B, '-r')
+plt.xlim(-1,1)
+plt.ylim(-1,1)
 
 plt.show()
--- champ_de_vitesse_3D.py
+++ champ_de_vitesse_3D.py
@@ -6,3 +6,27 @@ Created on Fri Aug 29 09:41:38 2025
 @author: lgcadillac
 """
 
+from Schemas import *
+import matplotlib.pyplot as plt
+import numpy as np
+from sys import exit
+
+ax = plt.figure().add_subplot(projection='3d')
+
+# Make the direction data for the arrows
+x, y, z = np.meshgrid(np.arange(-1., 1., 0.2),
+                      np.arange(-1., 1., 0.2),
+                      np.arange(-1., 1., 0.2))
+
+u = -y
+v = x
+w = -z
+
+def f(x,y,z):
+    return [-y,x,1+0.2*z]
+
+
+
+ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True, color = 'k')
+
+plt.show()
